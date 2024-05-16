0b9e5a8966e84e53cd04703c8064e3ab9ace326c
Pasha <pasha08072002@mail.ru>
2024-05-16 11:54:06 +0300

    add datetime

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from src.widget import types_cards_and_accounts
+from src.widget import types_cards_and_accounts, get_date
 
 number_card_or_account = input("Введите тип карты или счета и их номер ")
 print(types_cards_and_accounts(number_card_or_account))
 
+curr_date = input("Введите строку с датой ")
+print(get_date(curr_date))
--- src/widget.py
+++ src/widget.py
@@ -1,4 +1,6 @@
 from src.masks import changed_account_number, changed_card_number
+from datetime import datetime
+
 
 def types_cards_and_accounts(number_card_or_account: str) -> str:
     """Функция принимает на вход строку с информацией — тип карты/счета и номер карты/счета
@@ -11,3 +13,13 @@ def types_cards_and_accounts(number_card_or_account: str) -> str:
         split_num[-1] = changed_account_number(split_num[-1])
 
     return " ".join(split_num)
+
+
+def get_date(curr_date: str) -> str:
+    """
+    Функция принимает строку и возвращает строку с датой
+    """
+    date_interm = datetime.strptime(curr_date, "%Y-%m-%dT%H:%M:%S.%f")
+    date_output = datetime.strftime(date_interm, "%d.%m.%Y")
+
+    return date_output
